a011d5fa56ed3ee3d298ceff95a8d7acd0ec773a
enzoesper <enzoesper7@gmail.com>
2022-07-24 08:23:38 -0300

    hashing the password of the user (whenever creates or modify the password) before storing in the db.

--- auth/src/models/user.ts
+++ auth/src/models/user.ts
@@ -1,4 +1,5 @@
 import mongoose from "mongoose";
+import { Password } from "../services/password";
 
 // interface that describes properties required to create a new user
 interface UserAttrs {
@@ -27,6 +28,16 @@ const userSchema = new mongoose.Schema({
     required: true,
   },
 });
+
+userSchema.pre("save", async function (done) {
+  if (this.isModified("password")) {
+    const hashed = await Password.toHash(this.get("password"));
+    this.set("password", hashed);
+  }
+
+  done();
+});
+
 userSchema.statics.build = (attrs: UserAttrs) => {
   return new User(attrs);
 };
