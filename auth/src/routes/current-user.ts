78a12c48d291584b0354987f6cd5ab3bb2202c87
enzoesper <enzoesper7@gmail.com>
2022-07-30 22:50:14 -0300

    adding a middleware for requiring auth to route acces

--- auth/src/routes/current-user.ts
+++ auth/src/routes/current-user.ts
@@ -1,10 +1,11 @@
 import express, { Request, Response } from "express";
 import jwt from "jsonwebtoken";
 import { currenUser } from "../middlewares/current-user";
+import { requireAuth } from "../middlewares/require-auth";
 
 const router = express.Router();
 
-router.get("/api/users/currentuser", currenUser, (req: Request, res: Response) => {
+router.get("/api/users/currentuser", currenUser, requireAuth, (req: Request, res: Response) => {
   res.send({ curentUser: req.currentUser || null });
 });
 
