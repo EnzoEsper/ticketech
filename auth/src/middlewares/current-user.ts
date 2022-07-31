4c426acce61d7f2b0b57293908200803295b7d86
enzoesper <enzoesper7@gmail.com>
2022-07-30 21:37:02 -0300

    adding logic to the current user middleware and augmenting the type of the req to attach te curentUser propr

--- auth/src/middlewares/current-user.ts
+++ auth/src/middlewares/current-user.ts
@@ -1,13 +1,26 @@
 import { NextFunction, Request, Response } from "express";
 import jwt from "jsonwebtoken";
 
+interface UserPayload {
+  id: string;
+  email: string;
+}
+
+declare global {
+  namespace Express {
+    interface Request {
+      currentUser?: UserPayload;
+    }
+  }
+}
+
 export const currenUser = (req: Request, res: Response, next: NextFunction) => {
   if (!req.session?.jwt) {
     return next();
   }
 
   try {
-    const payload = jwt.verify(req.session.jwt!, process.env.JWT_KEY!);
+    const payload = jwt.verify(req.session.jwt!, process.env.JWT_KEY!) as UserPayload;
     req.currentUser = payload;
   } catch (error) {}
 
