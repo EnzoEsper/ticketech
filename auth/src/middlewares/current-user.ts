1fa3a333c0f38253e407e3601fa7cf246b73ae79
enzoesper <enzoesper7@gmail.com>
2022-07-30 21:29:15 -0300

    creating a currenUser middleware to extract the jwt payload and set it on req.CurrentUser

---
+++ auth/src/middlewares/current-user.ts
@@ -0,0 +1,15 @@
+import { NextFunction, Request, Response } from "express";
+import jwt from "jsonwebtoken";
+
+export const currenUser = (req: Request, res: Response, next: NextFunction) => {
+  if (!req.session?.jwt) {
+    return next();
+  }
+
+  try {
+    const payload = jwt.verify(req.session.jwt!, process.env.JWT_KEY!);
+    req.currentUser = payload;
+  } catch (error) {}
+
+  next();
+};
