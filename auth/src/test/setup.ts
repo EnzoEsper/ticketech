265adac455ff44c2c93c5e9dc4a0ddbd68959030
enzoesper <enzoesper7@gmail.com>
2022-08-06 23:21:28 -0300

    fixing the instance creation of the mongo memory server.

--- auth/src/test/setup.ts
+++ auth/src/test/setup.ts
@@ -4,7 +4,7 @@ import { app } from "../app";
 
 let mongo: any;
 beforeAll(async () => {
-  mongo = new MongoMemoryServer();
+  mongo = await MongoMemoryServer.create();
   const mongoUri = await mongo.getUri();
 
   await mongoose.connect(mongoUri);
