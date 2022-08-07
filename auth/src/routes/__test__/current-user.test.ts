1dbc39c9ac4dc4c59e29c6ec9436d4de373cdd88
enzoesper <enzoesper7@gmail.com>
2022-08-07 05:01:51 -0300

    adding authHelper to extract the cookie of a successfull signup and testing the currentuser route handler.

--- auth/src/routes/__test__/current-user.test.ts
+++ auth/src/routes/__test__/current-user.test.ts
@@ -1,17 +1,15 @@
 import request from "supertest";
 import { app } from "../../app";
+import { signup } from "../../test/authHelper";
 
 it("responds with details about the current user", async () => {
-  const authResponse = await request(app)
-    .post("/api/users/signup")
-    .send({
-      email: "test@test.com",
-      password: "password",
-    })
-    .expect(201);
-  const cookie = authResponse.get("Set-Cookie");
-
+  const cookie = await signup();
   const response = await request(app).get("/api/users/currentuser").set("Cookie", cookie).send().expect(200);
-
   expect(response.body.currentUser.email).toEqual("test@test.com");
+});
+
+it("responds with null when user is not authenticated", async () => {
+  const response = await request(app).get("/api/users/currentuser").send().expect(401);
+
+  expect(response.body.currentUser).toEqual(undefined);
 });
