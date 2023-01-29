a075cd15d8b0a19d86416753c1398174784d2747
Firstname Lastname <enzoesper7@gmail.com>
2023-01-29 16:57:06 -0300

    addign another getIinitalProps to the _app to pass current user data to the header and to the landingpage component (this has an issue: the getInitialProps is called ONLY in the _app and not in the index)

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -1,5 +1,16 @@
 import "bootstrap/dist/css/bootstrap.css";
+import { buildClient } from "../api/buildClient";
 
-export default ({ Component, pageProps }) => {
+const AppComponent = ({ Component, pageProps }) => {
   return <Component {...pageProps} />;
 };
+
+AppComponent.getInitialProps = async (appContext) => {
+  console.log("AppComponent.getInitialProps", appContext);
+  const client = buildClient(appContext.ctx);
+  const { data } = await client.get("/api/users/currentuser");
+
+  return data;
+};
+
+export default AppComponent;
