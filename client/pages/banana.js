63b4a55dbfa6af7828ceb47b91562ec741ff7442
Firstname Lastname <enzoesper7@gmail.com>
2023-01-29 16:33:34 -0300

    creating the signing component

--- client/pages/banana.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const banana = () => {
-  return <div>Banana3!</div>;
-};
-
-export default banana;
--- client/pages/index.js
+++ client/pages/index.js
@@ -3,9 +3,7 @@ import axios from "axios";
 import { buildClient } from "../api/buildClient";
 
 const LandingPage = ({ currentUser }) => {
-  console.log(currentUser);
-
-  return <h1>Landing Page</h1>;
+  return currentUser ? <h1>You are signed in</h1> : <h1>You are NOT signed id</h1>;
 };
 
 LandingPage.getInitialProps = async (context) => {
