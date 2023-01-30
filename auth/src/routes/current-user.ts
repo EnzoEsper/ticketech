cd994ae6a0baabbe6639f94c1f0cb19491a5d223
Firstname Lastname <enzoesper7@gmail.com>
2023-01-29 23:54:04 -0300

    fixing typo with currentUser

--- auth/src/routes/current-user.ts
+++ auth/src/routes/current-user.ts
@@ -1,11 +1,11 @@
 import express, { Request, Response } from "express";
 import jwt from "jsonwebtoken";
-import { currenUser } from "../middlewares/current-user";
+import { currentUser } from "../middlewares/current-user";
 import { requireAuth } from "../middlewares/require-auth";
 
 const router = express.Router();
 
-router.get("/api/users/currentuser", currenUser, requireAuth, (req: Request, res: Response) => {
+router.get("/api/users/currentuser", currentUser, requireAuth, (req: Request, res: Response) => {
   res.send({ currentUser: req.currentUser || null });
 });
 
