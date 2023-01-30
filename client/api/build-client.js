96870abaca0d45a3328669dc0d0141304e8c95b5
Firstname Lastname <enzoesper7@gmail.com>
2023-01-29 23:56:12 -0300

    renaming the file from buildClient to build-client

--- client/api/build-client.js
+++ client/api/build-client.js
@@ -1,16 +1,20 @@
 import axios from "axios";
 
-export const buildClient = ({ req }) => {
+const buildClient = ({ req }) => {
+  // console.log("headers", req.headers);
   if (typeof window === "undefined") {
-    // we are on the server
+    // We are on the server
+
     return axios.create({
       baseURL: "http://ingress-nginx-controller.ingress-nginx.svc.cluster.local",
       headers: req.headers,
     });
   } else {
-    // we are on the browser
+    // We must be on the browser
     return axios.create({
       baseURL: "/",
     });
   }
 };
+
+export default buildClient;
